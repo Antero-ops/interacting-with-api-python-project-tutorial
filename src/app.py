0522da8c18abd94489199a3f1d5da8054ce5db4a
Antero-ops <anterovasquez24@gmail.com>
2024-10-22 23:12:44 +0000

    cambios por acabar

--- src/app.py
+++ src/app.py
@@ -4,4 +4,11 @@ import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
+
+from dotenv import load_dotenv
 load_dotenv()
+
+import os
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
